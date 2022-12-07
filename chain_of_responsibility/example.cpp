9fa98d3c2a4692e3e1a4a6fb55ebc2a390c45958
Ryan <b04502132@ntu.edu.tw>
2022-12-07 15:50:36 +0800

    add example for COR

--- chain_of_responsibility/example.cpp
+++ chain_of_responsibility/example.cpp
@@ -3,29 +3,105 @@
 #include <string>
 using namespace std;
 
-class I_Handler
+class I_LogHandler
 {
     public:
-        virtual shared_ptr<I_Handler> setNextHandler(shared_ptr<I_Handler> handler) = 0;
-        virtual std::string requestHandle(std::string request) = 0;
+        enum LogLevel{INFO, WARN, ERROR, FATAL};
+        virtual shared_ptr<I_LogHandler> setNextLogHandler(shared_ptr<I_LogHandler> handler) = 0;
+        virtual void logMessage(int level, std::string message) = 0;
 };
-class BaseHandler : public I_Handler
+class LoggerHandler : public I_LogHandler
 {
     protected:
-        shared_ptr<I_Handler> next_handler_;
+        shared_ptr<I_LogHandler> next_handler_;
     public:
-        shared_ptr<I_Handler> setNextHandler(shared_ptr<I_Handler> handler) override
+        shared_ptr<I_LogHandler> setNextLogHandler(shared_ptr<I_LogHandler> handler) override
         {
             next_handler_ = handler;
             return handler;
         }
-        std::string requestHandle(std::string request) override
+        void logMessage(int level, std::string message) override
         {
-            return "Default handle";
+            if(level != LogLevel::INFO && level != LogLevel::WARN && level != LogLevel::ERROR && level != LogLevel::FATAL)
+                cout << "[Message] " << message << endl;
+            else
+                next_handler_->logMessage(level, message);
         }
 };
-
+class InfoLoggerHandler : public LoggerHandler
+{
+    public:
+        void logMessage(int level, std::string message) override
+        {
+            if(level == LogLevel::INFO)
+                cout << "[INFO] " << message << endl;
+            else
+                next_handler_->logMessage(level, message);
+        }
+};
+class WarnLoggerHandler : public LoggerHandler
+{
+    public:
+        void logMessage(int level, std::string message) override
+        {
+            if(level == LogLevel::WARN)
+                cout << "[WARN] " << message << endl;
+            else
+                next_handler_->logMessage(level, message);
+        }
+};
+class ErrorLoggerHandler : public LoggerHandler
+{
+    public:
+        void logMessage(int level, std::string message) override
+        {
+            if(level == LogLevel::ERROR)
+                cout << "[ERROR] " << message << endl;
+            else
+                next_handler_->logMessage(level, message);
+        }
+};
+class FatalLoggerHandler : public LoggerHandler
+{
+    public:
+        void logMessage(int level, std::string message) override
+        {
+            if(level == LogLevel::FATAL)
+                cout << "[FATAL] " << message << endl;
+            else
+                LoggerHandler::logMessage(level, message);
+        }
+};
+class LogSystem
+{
+    private:
+        shared_ptr<LoggerHandler> default_logger_;
+        shared_ptr<InfoLoggerHandler> info_logger_;
+        shared_ptr<WarnLoggerHandler> warn_logger_;
+        shared_ptr<ErrorLoggerHandler> error_logger_;
+        shared_ptr<FatalLoggerHandler> fatal_logger_;
+    public:
+        LogSystem()
+        {
+            default_logger_ = make_shared<LoggerHandler>();
+            info_logger_ = make_shared<InfoLoggerHandler>();
+            warn_logger_ = make_shared<WarnLoggerHandler>();
+            error_logger_ = make_shared<ErrorLoggerHandler>();
+            fatal_logger_ = make_shared<FatalLoggerHandler>();
+            default_logger_->setNextLogHandler(info_logger_)->setNextLogHandler(warn_logger_)->setNextLogHandler(error_logger_)->setNextLogHandler(fatal_logger_);
+        }
+        void LogMessage(LoggerHandler::LogLevel level, string message)
+        {
+            default_logger_->logMessage(level, message);
+        }
+};
 int main()
 {
+    auto Logger = make_shared<LogSystem>();
+    string message = "I am groot.";
+    Logger->LogMessage(LoggerHandler::INFO, message);
+    Logger->LogMessage(LoggerHandler::WARN, message);
+    Logger->LogMessage(LoggerHandler::ERROR, message);
+    Logger->LogMessage(LoggerHandler::FATAL, message);
     return 0;
 }
